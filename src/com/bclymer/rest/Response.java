15d0ab388485450cab1a834bb4c6cc126e13f230
Brian Clymer <crdnilfan@gmail.com>
2013-01-20 17:17:27 -0600

    Support not parsing the reponse

--- src/com/bclymer/rest/Response.java
+++ src/com/bclymer/rest/Response.java
@@ -1,16 +1,27 @@
 package com.bclymer.rest;
 
+import org.apache.http.Header;
+
 public class Response<T> {
 	
 	public T response;
-	public int statusCode;
+	public int httpStatusCode;
+	public int runningStatus;
 	public int errorCode;
 	public String rawResponse;
+	public Header[] headers;
 
 	public static class ErrorCodes {
 		public static final int NONE = 0;
 		public static final int CAST_ERROR = 1;
 		public static final int NETWORK_ERROR = 2;
 	}
+	
+	public static class RunningStatusCodes {
+		public static final int WAITING = 0;
+		public static final int RUNNING = 1;
+		public static final int FINISHED = 2;
+	}
+	
 	
 }
--- src/com/bclymer/rest/RestClient.java
+++ src/com/bclymer/rest/RestClient.java
@@ -2,20 +2,28 @@ package com.bclymer.rest;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RestClient {
@@ -34,24 +42,80 @@ public class RestClient {
         return instance;
     }
 	
-	public <T> T get(String url, long id, Class<T> clazz, RestCallback<T> callback) {
-		DownloadWebSourceTask<T> d = new DownloadWebSourceTask<T>(callback, new HttpGet(url), clazz);
+	public <T> void get(String url, long id, Class<T> clazz, RestCallback<T> callback) {
+		get(url, id, clazz, callback, null, null);
+	}
+	
+	public <T> void get(String url, long id, Class<T> clazz, RestCallback<T> callback, Header[] headers, HttpParams params) {
+		HttpGet request = new HttpGet(url);
+		if (headers != null) {
+			request.setHeaders(headers);
+		}
+		if (params != null) {
+			request.setParams(params);
+		}
+		DownloadWebSourceTask<T> d = new DownloadWebSourceTask<T>(callback, request, clazz);
 		mTasks.put(id, d);
 		d.execute();
-		return (T) null;
 	}
 
-	public <T> T post(String url) {
-		return (T) null;
+	public <T> void post(String url, long id, Class<T> clazz, RestCallback<T> callback) throws UnsupportedEncodingException, JsonProcessingException {
+		post(url, id, clazz, callback, null, null, null);
+	}
+	
+	public <T> void post(String url, long id, Class<T> clazz, RestCallback<T> callback, Header[] headers, HttpParams params, Object body) throws UnsupportedEncodingException, JsonProcessingException {
+		HttpPost request = new HttpPost(url);
+		if (headers != null) {
+			request.setHeaders(headers);
+		}
+		if (params != null) {
+			request.setParams(params);
+		}
+		if (body != null) {
+			StringEntity bodyString = new StringEntity(mMapper.writeValueAsString(body));
+			request.setEntity(bodyString);
+		}
+		DownloadWebSourceTask<T> d = new DownloadWebSourceTask<T>(callback, request, clazz);
+		mTasks.put(id, d);
+		d.execute();
 	}
 
-	public <T> T put(String url) {
-		return (T) null;
+	public <T> void put(String url, long id, Class<T> clazz, RestCallback<T> callback, Header[] headers, HttpParams params, Object body) throws UnsupportedEncodingException, JsonProcessingException {
+		HttpPut request = new HttpPut(url);
+		if (headers != null) {
+			request.setHeaders(headers);
+		}
+		if (params != null) {
+			request.setParams(params);
+		}
+		if (body != null) {
+			StringEntity bodyString = new StringEntity(mMapper.writeValueAsString(body));
+			request.setEntity(bodyString);
+		}
+		DownloadWebSourceTask<T> d = new DownloadWebSourceTask<T>(callback, request, clazz);
+		mTasks.put(id, d);
+		d.execute();
 	}
 
-	public <T> T delete(String url) {
-		
-		return (T) null;
+	public <T> void put(String url, long id, Class<T> clazz, RestCallback<T> callback) throws UnsupportedEncodingException, JsonProcessingException {
+		put(url, id, clazz, callback, null, null, null);
+	}
+
+	public <T> void delete(String url, long id, Class<T> clazz, RestCallback<T> callback, Header[] headers, HttpParams params) {
+		HttpDelete request = new HttpDelete(url);
+		if (headers != null) {
+			request.setHeaders(headers);
+		}
+		if (params != null) {
+			request.setParams(params);
+		}
+		DownloadWebSourceTask<T> d = new DownloadWebSourceTask<T>(callback, request, clazz);
+		mTasks.put(id, d);
+		d.execute();
+	}
+
+	public <T> void delete(String url, long id, Class<T> clazz, RestCallback<T> callback) {
+		delete(url, id, clazz, callback, null, null);
 	}
 	
 	private class DownloadWebSourceTask<T> extends AsyncTask<Void, Void, Response<T>> {
@@ -73,13 +137,17 @@ public class RestClient {
 	        try {
 	            httpResponse = mHttpClient.execute(request);
 	            StatusLine statusLine = httpResponse.getStatusLine();
-	            response.statusCode = statusLine.getStatusCode();
-	            if(response.statusCode == HttpStatus.SC_OK){
+	            response.httpStatusCode = statusLine.getStatusCode();
+	            if(response.httpStatusCode >= 200 && response.httpStatusCode <= 202){
 	                ByteArrayOutputStream out = new ByteArrayOutputStream();
 	                httpResponse.getEntity().writeTo(out);
+	                response.headers = httpResponse.getAllHeaders();
 	                out.close();
 	                response.rawResponse = out.toString();
-	                response.response = (T) mMapper.readValue(response.rawResponse, clazz);
+	                if (response.response instanceof Void == false && clazz != null) {
+	                	Log.e("", "Doing it");
+	                	response.response = (T) mMapper.readValue(response.rawResponse, clazz);
+	                }
 	                callback.onSuccess(response);
 	            } else{
 	                //Closes the connection.
